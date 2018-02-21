0c9abcd88ab435ad3971078c89e4be39c5cdaa5e
Vincent Desforges <desforges.vincent@gmail.com>
2018-02-21 17:29:37 +0000

    initialised the project still have to implement

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,32 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show]
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+4.times do
+  flats_attributes = {
+    name:             Faker::Address.community,
+    address:          Faker::Address.street_address,
+    description:      Faker::Lorem.paragraph(2),
+    price_per_night:  rand(25..300) ,
+    number_of_guests: rand(1..6)
+  }
+  flat = Flat.new(flats_attributes)
+  flat.save
+end
+
+puts 'Finished!'
